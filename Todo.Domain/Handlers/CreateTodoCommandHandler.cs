17761927e73f966af1b9fd1e84a0785698cbe461
Douglas Rodrigues <douglasrodrigues20161@outlook.com>
2021-02-10 22:23:20 -0300

    criando CreateTodoCommandHandler

---
+++ Todo.Domain/Handlers/CreateTodoCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using Flunt.Notifications;
+using Todo.Domain.Commands;
+using Todo.Domain.Commands.Contracts;
+using Todo.Domain.Entities;
+using Todo.Domain.Handlers.Contracts;
+using Todo.Domain.Repositories;
+
+namespace Todo.Domain.Handlers
+{
+    public class CreateTodoCommandHandler : Notifiable, IHandler<CreateTodoCommand>
+    {
+        private readonly ITodoRepository _todoRepository;
+
+        public CreateTodoCommandHandler(ITodoRepository todoRepository)
+        {
+            this._todoRepository = todoRepository;
+        }
+
+        public ICommandResult Handle(CreateTodoCommand command)
+        {
+            command.Validate();
+            if (command.Invalid)
+                return new GenericCommandResult(false, "Sua tarefa está errada!", command.Notifications);
+
+            TodoItem todo = new TodoItem(command.Title, command.Date, command.User);
+            this._todoRepository.Create(todo);
+            return new GenericCommandResult(true, "Tarefa criada com sucesso!", todo);
+        }
+    }
+}
